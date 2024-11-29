95dc5c7c04508a69ad90ae22fd65f4844a638df2
Mzamo Ntshaba <mzamontshaba14@gmail.com>
2024-11-29 17:59:15 +0200

    Created PodcastTile component to showcase individual podcast previews with details

---
+++ components/PodcastTile.tsx
@@ -0,0 +1,80 @@
+import { Preview, Genre } from '../utils/interfaces'
+
+interface PodcastTileProps {
+    propsPreview: Preview
+    propsGenres: Genre[]
+    onTileClick: (show: Preview) => void
+}
+
+export default function PodcastTile({
+    propsPreview,
+    propsGenres,
+    onTileClick,
+}: PodcastTileProps) {
+    const months = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
+    ]
+
+    const {
+        title,
+        seasons,
+        image,
+        genres,
+        updated: updatedDateStr,
+    } = propsPreview
+    const updatedDate = new Date(updatedDateStr)
+    const updated = `${updatedDate.getDate()} ${
+        months[updatedDate.getMonth()]
+    } ${updatedDate.getFullYear()}`
+
+    //Uses genre ids to find genrenames and add them as one string
+    const genreNames = genres
+        .map(
+            genreId =>
+                propsGenres.find(g => Number(g.id) === genreId)?.title || ''
+        )
+        .filter(Boolean)
+    const genreString = genreNames.join(', ')
+
+    //Handles click of a tile
+    const clickHandler = () => {
+        onTileClick(propsPreview)
+    }
+
+    return (
+        <div
+            onClick={clickHandler}
+            data-ref="preview-tile"
+            className="pt-6 pb-6 flex flex-col items-start rounded-xl bg-white cursor-pointer hover:opacity-75">
+            <img
+                src={image}
+                className="h-44 w-44 mb-5 self-center rounded-lg"
+                alt={title}
+            />
+            <div
+                data-ref="preview-content"
+                className="flex flex-col px-6 max-w-full">
+                <h1 className="text-slate-800 font-bold mb-2 text-nowrap truncate">
+                    {title}
+                </h1>
+                <hr className="mb-2" />
+                <p className="text-slate-800 text-sm">Seasons: {seasons}</p>
+                <p className="text-slate-800 text-sm">Genres: {genreString}</p>
+                <p className="text-slate-800 text-sm">
+                    Last updated: {updated}
+                </p>
+            </div>
+        </div>
+    )
+}
